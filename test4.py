647d446f85291686f33a66ecb1889f1ad28fddd2
nolan13as <nolan13as@icloud.com>
2019-06-02 19:19:00 +0300

    changes

--- test4.py
+++ test4.py
@@ -2,11 +2,19 @@
 
 num=0
 Sps=[]
-
-for i  in range(500,999,1) :
-    for k in range(500,999,1):
+NumMax=0
+p="Это самое большое число Палидром"
+n="Это самое большое число Палидром!!!"
+for i  in range(100,999,1) :
+    for k in range(100,999,1):
         num=i*k
         nam=str(num)
         Sps=list(nam)
-        if Sps[1]==Sps[-1] and Sps[2]==Sps[-2] and Sps[3]==Sps[-3]:
-            print(num)
+        if Sps[0]==Sps[-1] and Sps[1]==Sps[-2] and Sps[2]==Sps[-3]:
+            if NumMax<num:
+                NumMax=num
+                print(NumMax)
+                print(p)
+            else:
+                print(num)
+                print(n)

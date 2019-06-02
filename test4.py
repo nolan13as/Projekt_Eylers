bcce19142ba9e3d0802a532305ea000ee6b9ed3e
nolan13as <nolan13as@icloud.com>
2019-06-02 19:05:48 +0300

    changes

--- test4.py
+++ test4.py
@@ -8,8 +8,5 @@ for i  in range(500,999,1) :
         num=i*k
         nam=str(num)
         Sps=list(nam)
-        print(Sps)
         if Sps[1]==Sps[-1] and Sps[2]==Sps[-2] and Sps[3]==Sps[-3]:
             print(num)
-
-    

5f4b809c2c6aa047c6341365fec03bb19bd59e0c
nolan13as <nolan13as@icloud.com>
2019-06-02 17:37:25 +0300

    changes

--- test2.py
+++ test2.py
@@ -1,8 +1,18 @@
 ### Задача 2 ###
+
 last=0
 Fiba=[1,2]
+Summa=[]
 
 while last<=4000000:
     last=Fiba[-1]+Fiba[-2]
     Fiba.append(last)
     print(Fiba)
+
+Fiba.pop()
+print(Fiba)
+
+for i in Fiba :
+    if i%2==0:
+        Summa.append(i)
+        print(Summa)

e484a198e3fa8ce69c8d735c921ac4741d143565
RileyDixon <Den_Strelok@hotmail.com>
2019-06-02 18:06:22 +0300

    weferf

---
+++ test_2.py
@@ -0,0 +1,21 @@
+#Тест второй#
+last=0
+Fiba=[1,2]
+Summa=[]
+a=0
+
+while last<=4000000:
+  last=Fiba[-1]+Fiba[-2]
+  Fiba.append (last)
+  print(Fiba)
+
+Fiba.pop()
+print(Fiba)
+for i in Fiba:
+    if i%2==0:
+        Summa.append(i)
+        print(Summa)
+
+for i in Summa:
+    a+=i
+    print(a) 

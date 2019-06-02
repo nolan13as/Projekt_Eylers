9dcddd90467496a595dc8573966869f38a6f3419
nolan13as <nolan13as@icloud.com>
2019-06-02 17:46:55 +0300

    changes

--- test2.py
+++ test2.py
@@ -3,6 +3,7 @@
 last=0
 Fiba=[1,2]
 Summa=[]
+a=0
 
 while last<=4000000:
     last=Fiba[-1]+Fiba[-2]
@@ -12,7 +13,11 @@ while last<=4000000:
 Fiba.pop()
 print(Fiba)
 
-for i in Fiba :
+for i in Fiba:
     if i%2==0:
         Summa.append(i)
         print(Summa)
+
+for i in Summa:
+    a+=i
+    print(a)
